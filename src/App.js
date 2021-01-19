827e410b2397cf4c36f05224bc286ce013182bed
kylest0kes <kylestokes1551@gmail.com>
2021-01-18 20:22:05 -0500

    more tweaks

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <div className="App">
         <Header />
           <Switch>
-            <Route exact path='/' render={() => (
+            <Route exact path='/home' render={() => (
               <HomePage storeItems={storeItems}/>
             )} />
             <Route exact path='/cart' render={() => (
