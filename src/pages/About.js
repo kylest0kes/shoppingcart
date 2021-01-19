e28d9b4c12f37ab4614248eacad43a0a7fe60d52
kylest0kes <kylestokes1551@gmail.com>
2021-01-18 20:02:30 -0500

    deploy

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ export default function About() {
                 </div>
                 <div className="marketing-site-content-section-block">
                     <h3 className="marketing-site-content-section-block-header">PushCart</h3>
-                    <h5 className="project-desc">PushCart is a shopping cart application built with React and Redux. Used as a learning project, you are able to add items from the store into your cart, which then will display your items in the cart, their info, and a summary of how many items, and the total cost of your cart.</h5>
+                    <h5 className="project-desc">PushCart is a shopping cart application built with React and Redux. You are able to add items from the store into your cart, which then will display your items in the cart, their info, and a summary of how many items, and the total cost of your cart. The application also makes use of local state to persist the cart items on reload.</h5>
                     <h6 className="tech-used">Technologies Used:</h6>
                     <p className="project-desc">React.js, Redux, Foundation Framework</p>
                 </div>
