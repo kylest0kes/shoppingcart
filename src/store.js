47708d66770d59fe4d891c8f4fe882af832126ac
kylest0kes <kylestokes1551@gmail.com>
2021-01-18 19:46:12 -0500

    successfully implemented local storage to persist the cart items on reload

--- src/store.js
+++ src/store.js
@@ -12,15 +12,34 @@ function saveToLocalStorage(state) {
     }
 }
 
-const initState = {};
+function loadFromLocalStorage() {
+    console.log("in this bitchhh")
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined
+        } else {
+            return JSON.parse(serializedState);
+        }
+    } catch(err) {
+        console.log(err);
+        return undefined;
+    }
+}
+
 const middleware = [thunk];
+
+const persistedState = loadFromLocalStorage()
+;
 
 const store = createStore(
     rootReducer, 
-    initState,
-    composeWithDevTools(applyMiddleware(...middleware))
+    persistedState,
+    composeWithDevTools(applyMiddleware(...middleware)),
 )
 
-store.subscribe(() => saveToLocalStorage(store.getState()))
+store.subscribe(() => {
+    saveToLocalStorage(store.getState());
+});
 
 export default store;
